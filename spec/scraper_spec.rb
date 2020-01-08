419e3694b9e0a37b0f29fe90e8f4e80ca8882080
Yair fernando <yair.facio11@gmail.com>
2020-01-08 14:35:13 -0600

    Testing fetch_data from scraper.rb with rspec

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -1,11 +1,22 @@
 # frozen_string_literal: true
 
-require_relative './lib/scraper.rb'
+require './lib/scraper.rb'
 
 RSpec.describe 'RemoteWork Scraper' do
 
   describe '#fetch_data' do
-    it ''
+    scraper = Scraper.new
+    it 'when we fetch the data from the webside and do not get an empty array' do
+      expect(scraper.fetch_data).not_to be([])
+    end
+
+    it 'when we fetch the data from the webside and get one node' do
+      expect(scraper.fetch_data).not_to be([].size > 1)
+    end
+
+    it 'when we fetch the data from the webside and no not get a nil' do
+      expect(scraper.fetch_data).not_to be([].nil? == false)
+    end
   end
   
 
