ae59b5419270c0f6bc4b2e2841a939dbad1db113
Yair fernando <yair.facio11@gmail.com>
2020-01-08 14:43:35 -0600

    Testing fetch_job_listing from scraper.rb with rspec

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -3,9 +3,9 @@
 require './lib/scraper.rb'
 
 RSpec.describe 'RemoteWork Scraper' do
+  scraper = Scraper.new
 
   describe '#fetch_data' do
-    scraper = Scraper.new
     it 'when we fetch the data from the webside and do not get an empty array' do
       expect(scraper.fetch_data).not_to be([])
     end
@@ -18,6 +18,14 @@ RSpec.describe 'RemoteWork Scraper' do
       expect(scraper.fetch_data).not_to be([].nil? == false)
     end
   end
-  
 
+  describe '#fetch_job_listing' do
+    it 'when we fetch the job listing from the array and get another array with the job list' do
+      expect(scraper.fetch_job_listing([])).to eq([])
+    end
+
+    it 'when we fetch the job listing from the array and get more than 150 jobs in the list' do
+      expect(scraper.fetch_job_listing([])).not_to be([].size < 150)
+    end
+  end
 end
